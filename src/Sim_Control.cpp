c41857ab58cc60957419385b036e5c1a80ed67a9
Marvin Elz <MarvinElz@gmx.de>
2016-12-18 20:38:26 +0100

    Added Start- /Stopfunction for Controller

--- src/Sim_Control.cpp
+++ src/Sim_Control.cpp
@@ -1,23 +1,54 @@
 #include "ros/ros.h"
 #include "std_srvs/Empty.h"
+#include "sensor_msgs/Joy.h"
+#include "std_srvs/SetBool.h"
+
+bool run = true;
+bool running = false;
+
+void callback_rc_signal( const sensor_msgs::Joy::Ptr& msg )
+{     
+   if( msg->buttons[0]){
+	run = true;
+	if( !running )
+		ROS_INFO("START");
+   }
+   if( msg->buttons[1]){
+	run = false;	
+	if( running )
+		ROS_INFO("STOP");
+   }	
+}
 
 int main( int argc, char * argv[] ){
 
    ros::init(argc, argv, "Sim_Control");
    ros::NodeHandle nh("Sim_Control");
 
-   ros::ServiceClient dynamics_client = nh.serviceClient<std_srvs::Empty>("/Dynamics/dynamics_prop");
-   ros::ServiceClient control_client = nh.serviceClient<std_srvs::Empty>("/Controller/controller_prop");
+   ros::ServiceClient dynamics_client = nh.serviceClient<std_srvs::SetBool>("/Dynamics/dynamics_prop");
+   ros::ServiceClient control_client = nh.serviceClient<std_srvs::SetBool>("/Controller/controller_prop");
 
-   ros::Rate loop_rate(50);
+   ros::Subscriber sub_Signal = nh.subscribe( "/joy", 100, callback_rc_signal);
+
+   ros::Rate loop_rate(200);
 
    while(ros::ok())
-   {
-      std_srvs::Empty srv;
-      dynamics_client.call(srv);
-      control_client.call(srv);
-      ros::spinOnce();
-      loop_rate.sleep();
+   {	
+	std_srvs::SetBool srv;
+	if( run ){
+		if( !running ){
+			srv.request.data = true;
+			running = true;
+		}else{
+			srv.request.data = false;				
+		}		
+		dynamics_client.call(srv);
+      		control_client.call(srv);
+	}else{
+		running = false;
+	}	      	
+      	ros::spinOnce();
+      	loop_rate.sleep();
    }
 
    ros::spin();
